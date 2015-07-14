ed0dea1b5e6eb0e7d48e0865e4d32b7e4db65b71
Diqing Shu <saberdna@gmail.com>
2015-07-14 05:10:11 +0000

    set up routing

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
-var myApp = angular.module('myApp',[]);
+var artistControllers = angular.module('artistControllers',[]);
 
 // ['$scope','$http', ...] to protect from minify error
 
-myApp.controller('myController', ['$scope','$http', function($scope, $http){
+artistControllers.controller('ListController', ['$scope','$http', function($scope, $http){
     $http.get('js/data.json').success(function(data){
         $scope.artists = data;
         $scope.artistOrder = 'name';
-    }) 
+    });
 }]);
