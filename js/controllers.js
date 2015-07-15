8f2d6015a6e2a8b9923453de1f45b9ec15986139
Diqing Shu <saberdna@gmail.com>
2015-07-15 15:20:50 +0000

    Add animation to list

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var artistControllers = angular.module('artistControllers',[]);
+var artistControllers = angular.module('artistControllers',['ngAnimate']);
 
 // ['$scope','$http', ...] to protect from minify error
 
@@ -6,5 +6,29 @@ artistControllers.controller('ListController', ['$scope','$http', function($scop
     $http.get('js/data.json').success(function(data){
         $scope.artists = data;
         $scope.artistOrder = 'name';
+    });
+}]);
+
+// controller send var(artists and itemID) to $scope. in the $scope(template), use ng-model to sepcific data, and  itemID to get that piece of data
+// routeParams to get data emmberd in url(as string)
+
+artistControllers.controller('DetailsController', ['$scope','$http','$routeParams', function($scope, $http, $routeParams){
+    $http.get('js/data.json').success(function(data){
+        $scope.artists = data;
+        $scope.whichItem = $routeParams.itemId;
+        // prev
+        if ($routeParams.itemId > 0) {
+            $scope.prevItem = Number($routeParams.itemId)-1;
+        }else{
+            // to last item
+            $scope.prevItem = $scope.artists.length-1;
+        }
+        //next
+        if ($routeParams.itemId < $scope.artists.length-1) {
+            $scope.nextItem = Number($routeParams.itemId)+1;
+        }else{
+            // to first item
+            $scope.nextItem = 0;
+        }
     });
 }]);
